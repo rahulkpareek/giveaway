fbb73d5210449a24d23005da1c153a415bcac5da
rahulkpareek <r240825@gmail.com>
2024-12-25 19:52:43 +0100

    update FE

--- giveaway-frontend/src/App.js
+++ giveaway-frontend/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
-import Dashboard from './components/Dashboard';
 import Homepage from './components/Homepage';
 
+
 const App = () => {
-    const isLoggedIn = !!localStorage.getItem('token');
 
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Homepage />} />
-                {!isLoggedIn ? (
-                    <>
-                        <Route path="/register" element={<Register />} />
-                        <Route path="/login" element={<Login />} />
-                    </>
-                ) : (
-                    <Route path="/dashboard" element={<Dashboard />} />
-                )}
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </Router>
     );
