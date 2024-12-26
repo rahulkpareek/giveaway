fe43eb20b24dc8e0d9c1161f1e6cfda12e30a9e6
rahulkpareek <r240825@gmail.com>
2024-12-26 15:24:08 +0100

    update dashboard to use homepage.css instead of dashboard.css

--- giveaway-frontend/src/components/Dashboard.js
+++ giveaway-frontend/src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Dashboard.css'; // Add styles specific to Dashboard
+import './Homepage.css';
 import axios from 'axios';
 
 const Dashboard = () => {
@@ -28,7 +28,7 @@ const Dashboard = () => {
     }, [navigate]);
 
     return (
-        <div className="dashboard-container">
+        <div className="homepage-container">
             <nav className="navbar">
                 <div className="logo-container">
                     <div className="logo-text">Giveaway Dashboard</div>
